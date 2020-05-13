3db4d137f14c698a1afd507be5767fd3aa7f1d13
Radovan Slavkovsky <radovan.slavkovsky@gmail.com>
2020-05-13 20:17:37 +0200

    docker html

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/docker')
+def docker():
+    return render_template('docker.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
